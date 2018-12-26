b47c500b645ea78b55ff66a2696084172815e365
Remy Bohmer <linux@bohmer.net>
2018-12-26 14:45:28 +0100

    [Fix] Events are sometimes not based on Project Events
    Like the replication events. This makes the GSON parsing fail and
    causing the POST request to fail. This enforces Gerrit to keep on
    sending the same event over and over.
    
    Change-Id: I25cb8f8cd51d27de923ae51369498902bc476240
    Signed-off-by: Remy Bohmer <linux@bohmer.net>

--- src/main/java/jenkins/plugins/gerrit/GerritWebHook.java
+++ src/main/java/jenkins/plugins/gerrit/GerritWebHook.java
@@ -17,6 +17,7 @@ package jenkins.plugins.gerrit;
 import static hudson.model.Computer.threadPoolForRemoting;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import hudson.Extension;
 import hudson.model.RootAction;
 import hudson.model.UnprotectedRootAction;
@@ -66,6 +67,10 @@ public class GerritWebHook implements UnprotectedRootAction {
     HttpServletRequest req = Stapler.getCurrentRequest();
     GerritProjectEvent projectEvent = getBody(req);
 
+    if (projectEvent == null) {
+      return;
+    }
+
     log.info("GerritWebHook invoked for event " + projectEvent);
 
     List<WorkflowMultiBranchProject> jenkinsItems =
@@ -77,6 +82,7 @@ public class GerritWebHook implements UnprotectedRootAction {
       for (SCMSource scmSource : scmSources) {
         if (scmSource instanceof GerritSCMSource) {
           GerritSCMSource gerritSCMSource = (GerritSCMSource) scmSource;
+          log.debug("Checking match for SCM source: " + gerritSCMSource.getRemote());
           if (projectEvent.matches(gerritSCMSource.getRemote())) {
             log.info("Triggering SCM event for source " + scmSources.get(0) + " on job " + scmJob);
             scmJob.onSCMSourceUpdated(scmSource);
@@ -97,7 +103,12 @@ public class GerritWebHook implements UnprotectedRootAction {
         }
         String bodyString = stringBuilder.toString();
         log.info("Received body: " + bodyString);
-        return gson.fromJson(bodyString, GerritProjectEvent.class);
+        try {
+          return gson.fromJson(bodyString, GerritProjectEvent.class);
+        } catch (JsonSyntaxException e) {
+          log.debug("Not a Gerrit 'Project' Event, ignoring: " + bodyString);
+          return null;
+        }
       }
     }
   }
