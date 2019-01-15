23dced49257fea1a358ac4e8c3e1a77f6bdd25b5
Charles Padgett <cpadgett@stsci.edu>
2019-01-15 10:51:01 -0500

    Use getAllItems() in webhook to capture multibranch pipelines in folders
    Without this, the webhook will only find multibranch pipelines that are
    direct children of the Jenkins instance. Pipelines in folder structures
    will not be triggered.
    
    Added tests.
    
    Change-Id: I3be352f6187a41467c8a67117e85a078ac1b4d9b

---
+++ src/test/java/jenkins/plugins/gerrit/GerritWebHookTriggerTest.java
@@ -0,0 +1,89 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package jenkins.plugins.gerrit;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import com.cloudbees.hudson.plugins.folder.Folder;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import jenkins.branch.BranchSource;
+import jenkins.plugins.git.GitSCMSource;
+import jenkins.plugins.git.GitSampleRepoRule;
+import jenkins.plugins.git.traits.BranchDiscoveryTrait;
+import jenkins.scm.api.trait.SCMSourceTrait;
+import jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class GerritWebHookTriggerTest {
+
+  @Rule public JenkinsRule j = new JenkinsRule();
+  @Rule public GitSampleRepoRule g = new GitSampleRepoRule();
+
+  @Test
+  public void gerritWebHookTriggerNoFolderTest() throws Exception {
+
+    // setup multi-branch pipeline job at top level
+    WorkflowMultiBranchProject mp = j.jenkins.createProject(WorkflowMultiBranchProject.class, "p");
+
+    // now add a mocked-up GerritSCMSource and fire the gerrit-webhook
+    assertNull(mp.getIndexing().getResult());
+    GerritSCMSource mockSource = mock(GerritSCMSource.class);
+    when(mockSource.getRemote()).thenReturn("somerepo");
+    mp.getSourcesList().add(new BranchSource(mockSource));
+    String source = "{\"project\":{\"name\":\"somerepo\"}}";
+    HttpResponse<String> resp =
+        Unirest.post(j.jenkins.getRootUrl() + "gerrit-webhook/")
+            .header("Content-Type", "application/json")
+            .body(source)
+            .asString();
+
+    // make sure it triggered the build
+    j.waitUntilNoActivity();
+    assertNotNull(mp.getIndexing().getResult());
+  }
+
+  @Test
+  public void gerritWebHookTriggerFolderTest() throws Exception {
+
+    // setup multi-branch pipeline job in a folder
+    Folder f = j.jenkins.createProject(Folder.class, "folder" + j.jenkins.getItems().size());
+    WorkflowMultiBranchProject mp = f.createProject(WorkflowMultiBranchProject.class, "p");
+
+    // now add a mocked-up GerritSCMSource and fire the gerrit-webhook
+    assertNull(mp.getIndexing().getResult());
+    GerritSCMSource mockSource = mock(GerritSCMSource.class);
+    when(mockSource.getRemote()).thenReturn("somerepo");
+    mp.getSourcesList().add(new BranchSource(mockSource));
+    String source = "{\"project\":{\"name\":\"somerepo\"}}";
+    HttpResponse<String> resp =
+        Unirest.post(j.jenkins.getRootUrl() + "gerrit-webhook/")
+            .header("Content-Type", "application/json")
+            .body(source)
+            .asString();
+
+    // make sure it triggered the build
+    j.waitUntilNoActivity();
+    assertNotNull(mp.getIndexing().getResult());
+  }
+}
