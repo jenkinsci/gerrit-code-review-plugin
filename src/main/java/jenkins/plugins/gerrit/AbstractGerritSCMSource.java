c281391a27c420d8ed8c9de2413391e93942c073
Luca Milanesio <luca.milanesio@gmail.com>
2019-11-27 12:34:49 +0000

    Avoid fetching patch-sets multiple times
    Patch-set refs are immutable, there is no value in fetching them
    for detecting possible changes.
    
    Tidy up also messaging associated to fetching refs and possible
    exceptions.
    
    Change-Id: Ied45e28e76dec5022e0f780e30962362989e51de

--- src/main/java/jenkins/plugins/gerrit/AbstractGerritSCMSource.java
+++ src/main/java/jenkins/plugins/gerrit/AbstractGerritSCMSource.java
@@ -142,7 +142,6 @@ public abstract class AbstractGerritSCMSource extends AbstractGitSCMSource {
               throws IOException, InterruptedException {
 
             listener.getLogger().println("Checking " + remoteReferences.size() + " branches ...");
-            listener.getLogger().println("Checking " + remoteReferences.size() + " branches ...");
             Map<String, ObjectId> filteredRefs = filterRemoteReferences(remoteReferences);
             listener.getLogger().println("Filtered " + filteredRefs.size() + " branches ...");
             walk.setRetainBody(false);
@@ -165,7 +164,7 @@ public abstract class AbstractGerritSCMSource extends AbstractGitSCMSource {
                     return;
                   }
                 } catch (Exception e) {
-                  listener.getLogger().format("Unable to process $s: %s", refName, e.toString());
+                  listener.getLogger().format("Unable to process %s: %s", refName, e.toString());
                 }
               } else {
                 if (processBranchRequest(repository, walk, request, ref, listener)) {
@@ -577,7 +576,7 @@ public abstract class AbstractGerritSCMSource extends AbstractGitSCMSource {
 
       Changes.QueryRequest changeQuery = getOpenChanges(gerritApi, gerritURI.getProject());
 
-      List<RefSpec> fetchRefSpecs;
+      List<RefSpec> fetchRefSpecs = Collections.emptyList();
       try {
         if (head == null) {
           Stream<RefSpec> refSpecs =
@@ -587,21 +586,21 @@ public abstract class AbstractGerritSCMSource extends AbstractGitSCMSource {
                   .filter((RefSpec refSpec) -> !refSpec.getSource().contains(R_CHANGES));
           Stream<RefSpec> openChangesRefSpecs = changeQueryToRefSpecs(changeQuery);
           fetchRefSpecs = Stream.concat(refSpecs, openChangesRefSpecs).collect(Collectors.toList());
-        } else if (head instanceof ChangeSCMHead) {
+        } else if (!(head instanceof ChangeSCMHead)) {
+          String headName = head.getName();
           fetchRefSpecs =
               Arrays.asList(
-                  new RefSpec(
-                      R_CHANGES + head.getName() + ":refs/remotes/origin/" + head.getName()));
-        } else {
-          fetchRefSpecs = Collections.emptyList();
+                  new RefSpec("+refs/heads/" + headName + ":refs/remotes/origin/" + headName));
         }
       } catch (RestApiException e) {
         throw new IOException("Unable to fetch Gerrit open changes", e);
       }
 
-      listener.getLogger().println("Fetching refs " + fetchRefSpecs);
+      if(!fetchRefSpecs.isEmpty()) {
+        listener.getLogger().println("Fetching refs " + fetchRefSpecs);
+        fetch.from(remoteURI, fetchRefSpecs).execute();
+      }
 
-      fetch.from(remoteURI, fetchRefSpecs).execute();
       return retriever.run(client, context, remoteName, changeQuery);
     } finally {
       cacheLock.unlock();
