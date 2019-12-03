92d4657d4331d7746c7a7208a568583ce8702dbe
Thomas Draebing <thomas.draebing@sap.com>
2019-12-03 17:08:53 +0100

    Add REST API client for the checks plugin
    This change adds a REST API client for the checks plugin in preparation
    of adding the ability of the plugin to make use of the checks plugin.
    
    This client should be replaced by a client managed within the checks
    plugin itself in the future to reduce the copied code and to avoid
    incompatibilities, when the checks plugin changes.
    
    The discussion how to provide a REST client for the checks plugin is
    being tracked in Gerrit's bug tracker [1].
    
    [1] https://bugs.chromium.org/p/gerrit/issues/detail?id=12000
    
    Change-Id: I0ce5718ee89edde134acaa2aab57fa7f4e4c7fcc

---
+++ src/main/java/jenkins/plugins/gerrit/checks/api/BlockingCondition.java
@@ -0,0 +1,30 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package jenkins.plugins.gerrit.checks.api;
+
+import java.util.Set;
+
+/**
+ * Conditions evaluated on a check in the context of a change that determine whether the check
+ * blocks submission of a change.
+ */
+public enum BlockingCondition {
+  /** Block submission unless all required checks on the change is passing. */
+  STATE_NOT_PASSING;
+
+  public static Boolean isRequired(Set<BlockingCondition> blocking) {
+    return blocking.contains(STATE_NOT_PASSING);
+  }
+}
