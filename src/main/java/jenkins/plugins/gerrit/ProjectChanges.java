d0848011f6a855666ff57c889ac3db66090016ee
Grzegorz Golawski <grzegorz.golawski@nokia.com>
2019-12-20 17:34:16 +0100

    Support Gerrit < 2.15
    Check if Gerrit server version is < 2.15 and remove unsupported options before using changes API.
    
    Change-Id: Ib5441f29ec781326acb4b93fa8e9cc39f00ec637

--- src/main/java/jenkins/plugins/gerrit/ProjectChanges.java
+++ src/main/java/jenkins/plugins/gerrit/ProjectChanges.java
@@ -14,10 +14,14 @@
 
 package jenkins.plugins.gerrit;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.gerrit.extensions.api.GerritApi;
+import com.google.gerrit.extensions.client.ListChangesOption;
 import com.google.gerrit.extensions.common.ChangeInfo;
 import com.google.gerrit.extensions.restapi.RestApiException;
+import java.util.EnumSet;
 import java.util.Optional;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 class ProjectChanges {
@@ -31,11 +35,51 @@ class ProjectChanges {
 
   public Optional<ChangeInfo> get(int changeNumber) {
     try {
-      return Optional.ofNullable(gerritApi.changes().id(changeNumber).get());
+      EnumSet<ListChangesOption> options = EnumSet.allOf(ListChangesOption.class);
+      options.remove(ListChangesOption.CHECK);
+      if (isVersionBelow215(gerritApi.config().server().getVersion())) {
+        options.remove(ListChangesOption.TRACKING_IDS);
+        options.remove(ListChangesOption.SKIP_MERGEABLE);
+      }
+      return Optional.ofNullable(gerritApi.changes().id(changeNumber).get(options));
     } catch (RestApiException e) {
       LOGGER.severe(String.format("Unable to retrieve change %d", changeNumber));
       LOGGER.throwing(ProjectChanges.class.getName(), "get", e);
       return Optional.empty();
     }
+  }
+
+  @VisibleForTesting
+  boolean isVersionBelow215(String version) {
+    if (version == null) {
+      return false;
+    }
+
+    if (version.equals("<2.8")) {
+      return true;
+    }
+
+    String[] versionSplit = version.split("\\.");
+    if (versionSplit.length == 0) {
+      return false;
+    }
+    try {
+      int majorVersion = Integer.parseInt(versionSplit[0]);
+      if (versionSplit.length >= 1 && majorVersion < 2) {
+        return true;
+      }
+      if (versionSplit.length >= 1 && majorVersion > 2) {
+        return false;
+      }
+      int minorVersion = Integer.parseInt(versionSplit[1]);
+      if (versionSplit.length == 1 || minorVersion < 15) {
+        return true;
+      }
+    } catch (NumberFormatException e) {
+      LOGGER.log(Level.SEVERE, "Unable to part Gerrit version " + version, e);
+      return false;
+    }
+
+    return false;
   }
 }
