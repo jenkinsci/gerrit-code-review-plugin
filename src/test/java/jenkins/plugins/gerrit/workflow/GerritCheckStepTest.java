46dbe51cd1f863d8766393df4f099ac2e40ed78e
Joe Hansche <jhansche@themeetgroup.com>
2021-03-30 19:28:40 -0400

    [JENKINS-63437] Resolve improper timestamp format
    Gerrit expects timestamps in UTC timezone, but not in ISO-8601 format.
    Previously Gson was automatically formatting these in local time zone,
    but with no zone representation. Therefore when Gerrit displays the
    times in the UI, they are double-shifted.
    
    Change-Id: I80d59d51a9eeccd3ee9abe689fd32717a763441c

--- src/test/java/jenkins/plugins/gerrit/workflow/GerritCheckStepTest.java
+++ src/test/java/jenkins/plugins/gerrit/workflow/GerritCheckStepTest.java
@@ -14,6 +14,12 @@
 
 package jenkins.plugins.gerrit.workflow;
 
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.junit.Assert.assertThat;
+import static java.time.Instant.now;
+
 import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.CredentialsScope;
 import com.cloudbees.plugins.credentials.domains.Domain;
@@ -22,8 +28,11 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.google.gerrit.plugins.checks.api.CheckInput;
 import com.google.gerrit.plugins.checks.api.CheckState;
-import hudson.model.Result;
-import java.util.Collections;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
@@ -31,10 +40,18 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.mockserver.junit.MockServerRule;
+import org.mockserver.model.Format;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
 import org.mockserver.model.JsonBody;
 import org.mockserver.verify.VerificationTimes;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Collections;
+import hudson.model.Result;
 
 public class GerritCheckStepTest {
 
@@ -201,6 +218,117 @@ public class GerritCheckStepTest {
     String log = JenkinsRule.getLog(run);
 
     g.getClient().verify(HttpRequest.request(expectedUrl), VerificationTimes.once());
+  }
+
+  @Test
+  public void gerritCheckStepTestProducesUtcTimestamps() throws Exception {
+    long beginTestEpoch = now().getEpochSecond();
+    int changeId = 4321;
+    int revision = 1;
+    String checkerUuid = "checker";
+    String checkStatus = "RUNNING";
+    String branch = String.format("%02d/%d/%d", changeId % 100, changeId, revision);
+    UsernamePasswordCredentialsImpl c =
+        new UsernamePasswordCredentialsImpl(
+            CredentialsScope.GLOBAL, "cid", "cid", "USERNAME", "PASSWORD");
+    CredentialsProvider.lookupStores(j.jenkins)
+        .iterator()
+        .next()
+        .addCredentials(Domain.global(), c);
+    WorkflowJob p = j.jenkins.createProject(WorkflowJob.class, "p");
+    p.setDefinition(
+        new CpsFlowDefinition(
+            String.format(
+                ""
+                    + "node {\n"
+                    + "  withEnv([\n"
+                    + "    'GERRIT_API_URL=https://%s:%s/a/project',\n"
+                    + "    'GERRIT_API_INSECURE_HTTPS=true',\n"
+                    + "    'GERRIT_CREDENTIALS_ID=cid',\n"
+                    + "    'BRANCH_NAME=%s',\n"
+                    + "  ]) {\n"
+                    + "    gerritCheck checks: [%s: '%s']\n"
+                    + "  }\n"
+                    + "}",
+                g.getClient().remoteAddress().getHostString(),
+                g.getClient().remoteAddress().getPort(),
+                branch,
+                checkerUuid,
+                checkStatus),
+            true));
+
+    String expectedUrl = String.format("/a/changes/%s/revisions/%s/checks/", changeId, revision);
+
+    g.getClient()
+        .when(
+            HttpRequest.request(expectedUrl).withMethod("POST"))
+        .respond(
+            HttpResponse.response()
+                .withStatusCode(200)
+                .withBody(JsonBody.json(Collections.emptyMap())));
+
+    WorkflowRun run = j.assertBuildStatusSuccess(p.scheduleBuild2(0));
+    String log = JenkinsRule.getLog(run);
+
+    g.getClient().verify(HttpRequest.request(expectedUrl), VerificationTimes.once());
+    // It's very difficult to run verifications on the request body using verify(),
+    // but we can extract and parse the requests as JSON
+    String startedAt = requestBodyJson(expectedUrl).getAsJsonObject().get("started").getAsString();
+
+    // validate that the timestamp is in the proper format (not ISO-8601, not epoch millis, etc)
+    assertThat("Timestamp should be in SQL format",
+        startedAt, matchesPattern("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}\\.\\d{3}"));
+
+    // Whatever timestamp was formatted, validate that when it is parsed as a UTC time,
+    // it represents current time, +/- 10 seconds
+    long parsedEpoch = instantFromUtc(startedAt).getEpochSecond();
+    long endTestEpoch = now().getEpochSecond();
+    assertThat("Timestamp parsed as UTC should be between the time ranges of the test.",
+        parsedEpoch,
+            allOf(lessThanOrEqualTo(endTestEpoch), greaterThanOrEqualTo(beginTestEpoch)));
+  }
+
+  private static Instant instantFromUtc(String s) {
+    // Parse the "local"-format timestamp text as if it is UTC
+    DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+        .append(DateTimeFormatter.ISO_LOCAL_DATE)
+        .appendLiteral(' ')
+        .append(DateTimeFormatter.ISO_LOCAL_TIME)
+        .toFormatter()
+        .withZone(ZoneId.of("UTC"));
+    return ZonedDateTime.parse(s, formatter).toInstant();
+  }
+
+  /**
+   * Returns the JsonElement representation of the request body matching {@code requestUrl}.
+   *
+   * @param requestUrl the HttpRequest url to match
+   * @return {@link JsonElement} of the request body
+   */
+  private JsonElement requestBodyJson(String requestUrl) {
+    Gson gson = new Gson();
+
+    String request = g.getClient()
+        .retrieveRecordedRequests(HttpRequest.request(requestUrl), Format.JSON);
+    JsonElement el = gson.fromJson(request, JsonElement.class);
+    String innerBody = el.getAsJsonArray().get(0)
+        .getAsJsonObject().getAsJsonObject("body")
+        .get("string").getAsString();
+    return gson.fromJson(innerBody, JsonElement.class);
+  }
+
+  private static Matcher<String> matchesPattern(String pattern) {
+    return new TypeSafeMatcher<String>() {
+      @Override
+      protected boolean matchesSafely(String s) {
+        return s.matches(pattern);
+      }
+
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("matchesPattern /" + pattern + "/");
+      }
+    };
   }
 
   @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
