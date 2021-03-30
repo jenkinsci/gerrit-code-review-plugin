b83e923780525782c2c1f6dfb779f05dc3ab8c52
Joe Hansche <jhansche@themeetgroup.com>
2021-03-30 19:28:40 -0400

    [JENKINS-63437] Resolve improper timestamp format
    Gerrit expects timestamps in UTC timezone, but not in ISO-8601 format.
    Previously Gson was automatically formatting these in local time zone,
    but with no zone representation. Therefore when Gerrit displays the
    times in the UI, they are double-shifted.
    
    Change-Id: I80d59d51a9eeccd3ee9abe689fd32717a763441c

---
+++ src/main/java/com/google/gerrit/plugins/checks/client/UTCTimestampTypeAdapter.java
@@ -0,0 +1,53 @@
+package com.google.gerrit.plugins.checks.client;
+
+import static java.time.ZoneOffset.UTC;
+
+import com.google.gson.JsonParseException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * Format {@link java.sql.Timestamp} objects to JSON string representation compatible with the
+ * Gerrit API.
+ */
+class UTCTimestampTypeAdapter extends TypeAdapter<Date> {
+    private final DateFormat utcDateFormat;
+
+    public UTCTimestampTypeAdapter() {
+        super();
+        utcDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.US);
+        utcDateFormat.setTimeZone(TimeZone.getTimeZone(UTC));
+    }
+
+    @Override
+    public void write(JsonWriter out, Date date) throws IOException {
+        if (date == null) {
+            out.nullValue();
+        } else {
+            out.value(utcDateFormat.format(date));
+        }
+    }
+
+    @Override
+    public Date read(JsonReader in) throws IOException {
+        if (in.peek() == JsonToken.NULL) {
+            in.nextNull();
+            return null;
+        }
+
+        try {
+            return utcDateFormat.parse(in.nextString());
+        } catch (ParseException e) {
+            throw new JsonParseException(e);
+        }
+    }
+}
