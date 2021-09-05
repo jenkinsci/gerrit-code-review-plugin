c50c5eac8c77da628062501da6ad78fb599cc8e6
Luca Milanesio <luca.milanesio@gmail.com>
2021-09-05 22:55:00 +0100

    Fix timestamp parsing for Checks JSON payload
    The change I80d59d51a has introduced a fatal
    regression on the Checks JSON payload parsing
    because of an incompatibility of data-type of Date
    vs. Timestamp.
    
    Rectify the situation and allows the Checks feedback
    to be parsed correctly again, using the correct
    time-zone conversion.
    
    Change-Id: I441b04ec5fe337d83b16fb8c3246ce382a89df82

--- src/main/java/com/google/gerrit/plugins/checks/client/UTCTimestampTypeAdapter.java
+++ src/main/java/com/google/gerrit/plugins/checks/client/UTCTimestampTypeAdapter.java
@@ -8,10 +8,11 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -19,7 +20,7 @@ import java.util.TimeZone;
  * Format {@link java.sql.Timestamp} objects to JSON string representation compatible with the
  * Gerrit API.
  */
-class UTCTimestampTypeAdapter extends TypeAdapter<Date> {
+class UTCTimestampTypeAdapter extends TypeAdapter<Timestamp> {
   private final DateFormat utcDateFormat;
 
   public UTCTimestampTypeAdapter() {
@@ -29,7 +30,7 @@ class UTCTimestampTypeAdapter extends TypeAdapter<Date> {
   }
 
   @Override
-  public void write(JsonWriter out, Date date) throws IOException {
+  public void write(JsonWriter out, Timestamp date) throws IOException {
     if (date == null) {
       out.nullValue();
     } else {
@@ -38,14 +39,16 @@ class UTCTimestampTypeAdapter extends TypeAdapter<Date> {
   }
 
   @Override
-  public Date read(JsonReader in) throws IOException {
+  public Timestamp read(JsonReader in) throws IOException {
     if (in.peek() == JsonToken.NULL) {
       in.nextNull();
       return null;
     }
 
     try {
-      return utcDateFormat.parse(in.nextString());
+      Calendar utcCalendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+      utcCalendar.setTimeInMillis(utcDateFormat.parse(in.nextString()).getTime());
+      return Timestamp.from(utcCalendar.toInstant());
     } catch (ParseException e) {
       throw new JsonParseException(e);
     }
