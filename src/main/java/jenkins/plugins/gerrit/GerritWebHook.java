a02a89fc4dc6b93af4f50052c8cdfb2df160f84c
Luca Milanesio <luca.milanesio@gmail.com>
2023-03-15 15:43:00 +0000

    [SECURITY-2847] Introduce apiKey/jobName to WebHooks to prevent abuse
    The /gerrit-webhook/ endpoint allows the rescan of the SCM branches
    as soon as a Gerrit ref-update (or other event) is received, thanks to
    the integration with Gerrit web-hooks. However, the trigger can be also
    executed by a malicious user that could "guess" the project name by
    suffix and therefore cause a significant server overload due to the
    retriggering of the SCM scans.
    
    Introduce an additional apiKey parameter to the Gerrit SCM Source
    as an additional layer of security to prevent abuse by a malicious
    REST-API execution.
    
    P.S. The apiKey needs to be specified as URL query parameter because
    of the current lack of support of extra HTTP headers by Gerrit
    web-hooks.
    
    Also add a secondary 'jobName' in the query string for preventing
    the accidental matching of projects by SCM source suffixes: only the
    multi-branch pipeline matching exactly the job name specified will
    be considered for triggering the SCM source events.
    
    NOTE: This is a breaking change for existing job definitions
    and webhooks configuration because the apiKey is a mandatory
    parameter, its absence would cause the webhook to fail and existing
    jobs to miss the automatic triggering.
    
    Change-Id: I55ceb10a00981f6c0f889616ee906f1d002782cb

--- src/main/java/jenkins/plugins/gerrit/GerritWebHook.java
+++ src/main/java/jenkins/plugins/gerrit/GerritWebHook.java
@@ -15,6 +15,7 @@
 package jenkins.plugins.gerrit;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Strings;
 import com.google.common.collect.Sets;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -24,16 +25,18 @@ import hudson.model.RootAction;
 import hudson.model.UnprotectedRootAction;
 import hudson.security.ACL;
 import hudson.security.ACLContext;
+import hudson.util.Secret;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.servlet.http.HttpServletRequest;
 import jenkins.model.Jenkins;
-import jenkins.scm.api.SCMSource;
 import jenkins.scm.api.SCMSourceOwner;
 import org.acegisecurity.Authentication;
 import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
@@ -76,6 +79,10 @@ public class GerritWebHook implements UnprotectedRootAction {
   @SuppressWarnings({"unused", "deprecation"})
   public void doIndex() throws IOException {
     HttpServletRequest req = Stapler.getCurrentRequest();
+    String jobName = req.getParameter("jobName");
+    boolean isJobNameNullOrEmpty = Strings.isNullOrEmpty(jobName);
+    String apiKeyParam = req.getParameter("apiKey");
+
     getBody(req)
         .ifPresent(
             projectEvent -> {
@@ -89,28 +96,70 @@ public class GerritWebHook implements UnprotectedRootAction {
 
               try (ACLContext acl = ACL.as(ACL.SYSTEM)) {
                 List<WorkflowMultiBranchProject> jenkinsItems =
-                    getJenkinsInstance().getAllItems(WorkflowMultiBranchProject.class);
-                log.info("Scanning {} Jenkins items", jenkinsItems.size());
-                for (SCMSourceOwner scmJob : jenkinsItems) {
-                  log.info("Scanning job " + scmJob);
-                  List<SCMSource> scmSources = scmJob.getSCMSources();
-                  for (SCMSource scmSource : scmSources) {
-                    if (scmSource instanceof GerritSCMSource) {
-                      GerritSCMSource gerritSCMSource = (GerritSCMSource) scmSource;
-                      log.debug("Checking match for SCM source: " + gerritSCMSource.getRemote());
-                      if (projectEvent.matches(gerritSCMSource.getRemote())) {
-                        log.info(
-                            "Triggering SCM event for source "
-                                + scmSources.get(0)
-                                + " on job "
-                                + scmJob);
-                        scmJob.onSCMSourceUpdated(scmSource);
-                      }
-                    }
+                    getJenkinsInstance()
+                        .getAllItems(WorkflowMultiBranchProject.class)
+                        .stream()
+                        .filter(job -> isJobNameNullOrEmpty || job.getName().equals(jobName))
+                        .collect(Collectors.toList());
+                if (!isJobNameNullOrEmpty) {
+                  if (jenkinsItems.isEmpty()) {
+                    log.error("Job '{}' not found or not a multi-branch pipeline", jobName);
+                    return;
+                  }
+
+                  if (jenkinsItems.size() > 1) {
+                    log.error(
+                        "Search for job '{}' is ambiguous and returned {} entries",
+                        jobName,
+                        jenkinsItems.size());
+                    return;
                   }
                 }
+
+                log.info(
+                    "Scanning {} Jenkins items {}",
+                    jenkinsItems.size(),
+                    jobName == null ? "" : "matching " + jobName);
+
+                jenkinsItems.forEach(
+                    scmJob ->
+                        scmJob
+                            .getSCMSources()
+                            .stream()
+                            .filter(GerritSCMSource.class::isInstance)
+                            .map(GerritSCMSource.class::cast)
+                            .forEach(
+                                scmSource ->
+                                    triggerScmSourceOnJob(
+                                        apiKeyParam, projectEvent, scmJob, scmSource)));
               }
             });
+  }
+
+  private void triggerScmSourceOnJob(
+      String apiKeyParam,
+      GerritProjectEvent projectEvent,
+      SCMSourceOwner scmJob,
+      GerritSCMSource gerritSCMSource) {
+    Secret gerritSCMSourceApiKey = gerritSCMSource.getApiKey();
+    log.debug("Checking match for SCM source: " + gerritSCMSource.getRemote());
+    if (!projectEvent.matches(gerritSCMSource.getRemote())) {
+      log.warn(
+          "Not triggering job {}: SCM source remote does not match the one specified in the project event",
+          scmJob.getName());
+      return;
+    }
+
+    if (Secret.toString(gerritSCMSourceApiKey).isEmpty()) {
+      log.debug("The apiKey secret was not configured in the SCM source or empty");
+    } else if (!Objects.equals(apiKeyParam, gerritSCMSourceApiKey.getPlainText())) {
+      log.error(
+          "Unable to trigger the SCM source because of the ApiKey provided in gerrit web-hook does not match the one configured in the source");
+      return;
+    }
+
+    log.info("Triggering SCM event for source {} on job {}", gerritSCMSource, scmJob);
+    scmJob.onSCMSourceUpdated(gerritSCMSource);
   }
 
   @VisibleForTesting
